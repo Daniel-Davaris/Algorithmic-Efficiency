185b015e061c88104afed583b5ea47bc585c00f8
Daniel-Davaris <danieldavarispriv@gmail.com>
2019-02-28 13:22:44 +1100

    This is the combined graphs however the output of the graph does not reflect what is actually going on

--- sorting3.py
+++ sorting3.py
@@ -1,34 +1,30 @@
 import matplotlib.pyplot as plt 
 import math 
 import random
+import re
 from timeit import default_timer as timer
 
 # input sizes
-input_sizes = [5,10,20,50,100,500,1000]
+input_sizes = [5,10,50,100,500,1000]
 
 # definitions for our yvals and xvals 
 yvals = []
 xvals = [i for i in input_sizes]
 
 # average_times will be stored for each input_size
-average_times_a = []
+average_times = []
 
-"""
 
-average_times_b = []
-
-"""
-
-def selectionSort(my_list_a):
+def selectionSort(my_data):
     # perform sort
     # find lowest value in list - continue while we still have values
-    while len(my_list_a) > 0:
+    while len(my_data) > 0:
         
         # each time through reset the lowest value
         lowest = None
 
         # step through list, looking for lowest value
-        for item in my_list_a:
+        for item in my_data:
             if lowest == None or lowest > item:
                 lowest = item
 
@@ -36,13 +32,13 @@ def selectionSort(my_list_a):
         sorted_data.append(lowest)
 
         # remove the value from the original list
-        my_list_a.remove(lowest)
+        my_data.remove(lowest)
 
         return sorted_data
 
-"""
+
 # Our mergeSort function that takes in 'my_list' as the parrameter 
-def mergeSort(my_list_b):                               # T(n)
+def mergeSort(my_data):                               # T(n)
 
     # function variables
     sorted_list = []                                    # 1
@@ -50,14 +46,14 @@ def mergeSort(my_list_b):                               # T(n)
     # sorting stage
 
     # terminating case (n = 1)
-    if len(my_list_b) == 1:
-        return my_list_b                                   # 1 i.e. T(1) == O(1)
+    if len(my_data) == 1:
+        return my_data                                    # 1 i.e. T(1) == O(1)
 
     # general case
     else:
-        mid = int(len(my_list_b) / 2)                       # 1
-        lefthalf = my_list_b[:mid]                          # 1
-        righthalf = my_list_b[mid:]                         # 1
+        mid = int(len(my_data) / 2)                       # 1
+        lefthalf = my_data[:mid]                          # 1
+        righthalf = my_data[mid:]                         # 1
 
         lefthalf = mergeSort(lefthalf)                    # T(n/2)
         righthalf = mergeSort(righthalf)                  # T(n/2)
@@ -89,100 +85,68 @@ def mergeSort(my_list_b):                               # T(n)
             j=j+1
 
         return sorted_list
-"""
-# analysis stage of the code 
-# run trial on each input size
-for size in input_sizes:
 
-    # record the times taken for each input size
-    times = []
-    # perform 20 trials
-    for trial_number in range(20):
-        # construct a random set of input data
-        my_list_a = []
-        for i in range(size):
-            my_list_a.append(random.randint(-100,100))
-        sorted_data = [] # blank list to hold sorted values
+li = [selectionSort, mergeSort]
 
-       # begin timer for selection sort 
-        start = timer()
+for func in li:
 
-        selectionSort(my_list_a)    #   executes the analysis on the function 
+    # analysis stage of the code 
+    # run trial on each input size
+    for size in input_sizes:
 
-        # end timer
-        end = timer()
+        # record the times taken for each input size
+        times = []
+        # perform 20 trials
+        for trial_number in range(20):
+            # construct a random set of input data
+            my_data = []
+            for i in range(size):
+                my_data.append(random.randint(-100,100))
+            sorted_data = [] # blank list to hold sorted values
 
-        # calculate running time (measured in seconds)
-        running_time = end - start
-        # add it to our list of times
-        times.append(running_time)
-        # print(running_time)
+            # begin timer for selection sort 
+            start = timer()
 
-    # sorted_data is now the sorted version of the original list
-    # print("result", sorted_data)  # uncomment this line to see the sorted version
-    # calculate the average time and append to our list of average_times
-    average_times_a.append(sum(times) / float(len(times)))  # float just in case python 2 is used
+            func(my_data)    #   executes the analysis on the function 
 
-# print average "running times" for each input size
-for i in range(len(input_sizes)):
-    print("Running time for input size",input_sizes[i], "is", average_times_a[i])
-print(average_times_a, xvals)
-yvals = []
-"""
+            # end timer
+            end = timer()
 
-# analysis stage of the code 
-# run trial on each input size
-for size in input_sizes:
+            # calculate running time (measured in seconds)
+            running_time = end - start
+            # add it to our list of times
+            times.append(running_time)
+            # print(running_time)
 
-    # record the times taken for each input size
-    times = []
-    # perform 20 trials
-    for trial_number in range(20):
-        # construct a random set of input data
-        my_list_b = []
-        for i in range(size):
-            my_list_b.append(random.randint(-100,100))
-        sorted_data = [] # blank list to hold sorted values
+        # sorted_data is now the sorted version of the original list
+        # print("result", sorted_data)  # uncomment this line to see the sorted version
+        # calculate the average time and append to our list of average_times
+        average_times.append(sum(times) / float(len(times)))  # float just in case python 2 is used
 
-       # begin timer for selection sort 
-        start = timer()
+    # print average "running times" for each input size
+    for i in range(len(input_sizes)):
+        print("Running time for input size",input_sizes[i], "is", average_times[i])
+    print(average_times, xvals)
 
-        mergeSort(my_list_b)    #   executes the analysis on the function 
 
-        # end timer
-        end = timer()
 
-        # calculate running time (measured in seconds)
-        running_time = end - start
-        # add it to our list of times
-        times.append(running_time)
-        # print(running_time)
 
-    # sorted_data is now the sorted version of the original list
-    # print("result", sorted_data)  # uncomment this line to see the sorted version
-    # calculate the average time and append to our list of average_times
-    average_times_b.append(sum(times) / float(len(times)))  # float just in case python 2 is used
+    func_name = re.search(r"<function (.*?) at (.*?)>", str(func)).group(1)
 
-# print average "running times" for each input size
-for i in range(len(input_sizes)):
-    print("Running time for input size",input_sizes[i], "is", average_times_b[i])
+    plt.plot(xvals, average_times, label=str(func_name))
 
-#print(average_times, xvals)
+    average_times = []
 
-"""
-
-plt.plot(xvals, average_times_a, label="Selection Sort")
 
 yvals = []
 
 """
-
-plt.plot(xvals, average_times_b, label="Merge Sort")
+for n in xvals:
+    yvals.append(n**2)
+plt.plot(xvals, yvals, label="y = x^2")
 
 yvals = []
-
 """
-
 plt.xlabel('Input Size')
 plt.ylabel('Average Running Time')
 
